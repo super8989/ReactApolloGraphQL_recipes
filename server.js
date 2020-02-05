92a3d18007fb39a05d350a940253ff21f519419c
Sam <super8989@gmail.com>
2020-02-04 21:09:16 -0500

    create Mongoose schemas

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
 require('dotenv').config({ path: 'variables.env' });
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
 
+// Connects to database
 mongoose
-	.connect(process.env.MONGO_URI)
+	.connect(process.env.MONGO_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	})
 	.then(() => console.log('DB connected'))
 	.catch(err => console.error(err));
 
+// Initializes application
 const app = express();
 
 const PORT = process.env.PORT || 4444;
